1c74ce7cbfa343cf08259d2c5c64883546007403
Adriano Amaricci <adriano.amaricci@gmail.com>
2016-04-04 14:18:08 +0200

    v4.3.1b:
    - small update of the BHZ 2D Edge states code.

--- drivers/ed_bhz_2d_edge.f90
+++ drivers/ed_bhz_2d_edge.f90
@@ -6,35 +6,38 @@ program ed_bhz_2d_edge
   USE MPI
 #endif
   implicit none
+
   integer                                       :: iloop
   integer                                       :: Nlso
   integer                                       :: Nso
-  integer                                       :: ilat,iy,iorb,ispin
+  integer                                       :: Nineq
+  integer                                       :: ilat,iy,iorb,ispin,ineq
   logical                                       :: converged
   !Bath:
   integer                                       :: Nb
-  real(8),allocatable                           :: Bath(:,:),Bath_Prev(:,:)
+  real(8),allocatable,dimension(:,:)            :: Bath
+  real(8),allocatable,dimension(:,:)            :: Bath_ineq
+  real(8),allocatable,dimension(:,:)            :: Bath_prev
   !The local hybridization function:
   complex(8),allocatable,dimension(:,:,:,:,:,:) :: Weiss
-  complex(8),allocatable,dimension(:,:,:,:,:,:) :: Smats
-  complex(8),allocatable,dimension(:,:,:,:,:,:) :: Sreal
-  complex(8),allocatable,dimension(:,:,:,:,:,:) :: Gmats
+  complex(8),allocatable,dimension(:,:,:,:,:,:) :: Smats,Smats_ineq
+  complex(8),allocatable,dimension(:,:,:,:,:,:) :: Sreal,Sreal_ineq
+  complex(8),allocatable,dimension(:,:,:,:,:,:) :: Gmats,Gmats_ineq
   complex(8),allocatable,dimension(:,:,:,:,:,:) :: Greal
   !hamiltonian input:
   complex(8),allocatable,dimension(:,:,:)       :: Hkr
   complex(8),allocatable,dimension(:,:)         :: bhzHloc
-  complex(8),allocatable,dimension(:,:,:,:,:)   :: Hloc,S0
+  complex(8),allocatable,dimension(:,:,:,:,:)   :: Hloc,Hloc_ineq,S0
   !gamma matrices:
   complex(8),allocatable,dimension(:,:)         :: gamma1
   complex(8),allocatable,dimension(:,:)         :: gamma2
   complex(8),allocatable,dimension(:,:)         :: gamma5
-
-  real(8),allocatable,dimension(:)              :: Wtk,wm,wr
+  real(8),allocatable,dimension(:)              :: Wtk
   real(8),allocatable,dimension(:)              :: kxgrid
-  real(8),dimension(:,:),allocatable            :: dens,rho,kpath
+  real(8),dimension(:,:),allocatable            :: kpath
   integer                                       :: Nk,Ly,Nkpath
   real(8)                                       :: e0,mh,lambda,wmixing
-  logical                                       :: spinsym,tridiag,getsigma
+  logical                                       :: spinsym,tridiag,lrsym
   character(len=60)                             :: finput
   character(len=32)                             :: hkfile
 
@@ -56,14 +59,21 @@ program ed_bhz_2d_edge
   call parse_input_variable(mh,"MH",finput,default=1d0)
   call parse_input_variable(lambda,"LAMBDA",finput,default=0.3d0)
   call parse_input_variable(e0,"e0",finput,default=1d0)
+  call parse_input_variable(lrsym,"LRSYM",finput,default=.true.)
   call parse_input_variable(spinsym,"SPINSYM",finput,default=.true.)
-  call parse_input_variable(getsigma,"GETSIGMA",finput,default=.false.)
   call parse_input_variable(wmixing,"WMIXING",finput,default=0.5d0)
   !
   call ed_read_input(trim(finput))
 
   !set the global number of lattice sites equal to the number of layers along the y-axis
   Nlat = Ly
+  Nineq= Ly
+  if(lrsym)then
+     if(mod(Ly,2)/=0)stop "Wrong setup from input file: Ly%2 > 0 (odd number of sites)"
+     Nineq=Ly/2
+     print*,"Using L-R Symmetry. Solve",Nineq," of",Nlat," sites."
+     call sleep(2)
+  endif
 
   !set the local number of total spin-orbitals (4)
   if(Nspin/=2.OR.Norb/=2)stop "Wrong setup from input file: Nspin=Norb=2 -> 4Spin-Orbitals"
@@ -72,16 +82,20 @@ program ed_bhz_2d_edge
   !set the total lattice-spin-orbit dimension:
   Nlso=Nlat*Nspin*Norb
 
-  !Allocate Weiss Field:
-  allocate(Weiss(Nlat,Nspin,Nspin,Norb,Norb,Lmats))
+
+
+  !Allocate Functions:
+  allocate(Weiss(Nineq,Nspin,Nspin,Norb,Norb,Lmats))
   allocate(Smats(Nlat,Nspin,Nspin,Norb,Norb,Lmats))
-  allocate(Gmats(Nlat,Nspin,Nspin,Norb,Norb,Lmats))
   allocate(Sreal(Nlat,Nspin,Nspin,Norb,Norb,Lreal))
+  allocate(Gmats(Nlat,Nspin,Nspin,Norb,Norb,Lmats))
   allocate(Greal(Nlat,Nspin,Nspin,Norb,Norb,Lreal))
   allocate(Hloc(Nlat,Nspin,Nspin,Norb,Norb))
+  !
+  allocate(Smats_ineq(Nineq,Nspin,Nspin,Norb,Norb,Lmats))
+  allocate(Sreal_ineq(Nineq,Nspin,Nspin,Norb,Norb,Lreal))
+  allocate(Hloc_ineq(Nineq,Nspin,Nspin,Norb,Norb))
   allocate(S0(Nlat,Nspin,Nspin,Norb,Norb))
-  allocate(dens(Ly,Norb),rho(Ly,Norb))
-  allocate(wm(Lmats),wr(Lreal))
   S0=zero
 
 
@@ -89,61 +103,76 @@ program ed_bhz_2d_edge
   !Buil the Hamiltonian on a grid or on  path
   call build_hkr(trim(hkfile))
   Hloc = lso2nnn_reshape(bhzHloc,Nlat,Nspin,Norb)
-
+  do ineq=1,Nineq
+     ilat = ineq2ilat(ineq)
+     Hloc_ineq(ineq,:,:,:,:) = Hloc(ilat,:,:,:,:)
+  enddo
 
 
   !Setup solver
   Nb=get_bath_size()
-  allocate(Bath(Nlat,Nb), Bath_Prev(Nlat,Nb) )
-  call ed_init_solver_lattice(bath)
+  allocate(Bath(Nlat,Nb) )
+  allocate(Bath_ineq(Nineq,Nb) )
+  allocate(Bath_prev(Nineq,Nb) )
+  call ed_init_solver_lattice(Bath)
+  do ineq=1,Nineq
+     ilat = ineq2ilat(ineq)
+     Bath_ineq(ineq,:) = Bath(ilat,:)
+  enddo
 
 
-  if(getsigma)then
-     print*,"Entering ed_solve_lattice."
-     call ed_solve_lattice(bath,Hloc,iprint=1)
-     call ed_get_sigma_matsubara_lattice(Smats,Nlat)
-     S0 = Smats(:,:,:,:,:,1)
-     allocate(kpath(3,1))
-     kpath(1,1)=-pi/2
-     kpath(2,1)= 0d0
-     kpath(3,1)= pi/2
-     if(mpiID==0)call build_eigenbands(kpath)
-#ifdef _MPI_INEQ
-     call MPI_FINALIZE(mpiERR)
-#endif
-     stop
-  endif
 
   !DMFT loop:
   iloop=0 ; converged=.false.
   do while(.not.converged.AND.iloop<nloop)
      iloop=iloop+1
      if(mpiID==0) call start_loop(iloop,nloop,"DMFT-loop")   
-     bath_prev=bath
-     ! solve the impurities on each y-layer
-     call ed_solve_lattice(bath,Hloc,iprint=1)
+
+     ! solve the impurities on each inequivalent y-layer
+     call ed_solve_lattice(Bath_ineq,Hloc_ineq,iprint=1)
+
      ! retrieve the self-energies
-     call ed_get_sigma_matsubara_lattice(Smats,Nlat)
-     call ed_get_sigma_real_lattice(Sreal,Nlat)
-     S0 = Smats(:,:,:,:,:,1)
+     ! store the 1st Matsubara freq. into S0, used to get H_topological = Hk + S0
+     call ed_get_sigma_matsubara_lattice(Smats_ineq,Nineq)
+     call ed_get_sigma_real_lattice(Sreal_ineq,Nineq)
+     do ilat=1,Nlat
+        ineq = ilat2ineq(ilat)
+        Smats(ilat,:,:,:,:,:) = Smats_ineq(ineq,:,:,:,:,:)
+        Sreal(ilat,:,:,:,:,:) = Sreal_ineq(ineq,:,:,:,:,:)
+        S0(ilat,:,:,:,:)      = Smats_ineq(ineq,:,:,:,:,1)
+     enddo
+
+
      ! compute the local gf:
      call ed_get_gloc_lattice(Hkr,Wtk,Gmats,Greal,Smats,Sreal,iprint=1,tridiag=tridiag)
-     ! compute the Weiss field
-     call ed_get_weiss_lattice(Gmats,Smats,Weiss,Hloc,iprint=1)
+     do ineq=1,Nineq
+        ilat = ineq2ilat(ineq)
+        Gmats_ineq(ineq,:,:,:,:,:) = Gmats(ilat,:,:,:,:,:)
+     enddo
+
+
+     ! compute the Weiss field (only the Nineq ones)
+     call ed_get_weiss_lattice(Gmats_ineq,Smats_ineq,Weiss,Hloc_ineq,iprint=1)
+
+
      ! fit baths and mix result with old baths
-     call ed_chi2_fitgf_lattice(bath,Weiss,Hloc,ispin=1)
-     if(.not.spinsym)then
-        call ed_chi2_fitgf_lattice(bath,Weiss,Hloc,ispin=2)
-     else
-        do ilat=1,Nlat
-           call spin_symmetrize_bath(bath(ilat,:))
+     call ed_chi2_fitgf_lattice(Bath_ineq,Weiss,Hloc_ineq,ispin=1)
+     if(spinsym)then
+        do ineq=1,Nineq
+           call spin_symmetrize_bath(Bath_ineq(ineq,:))
         enddo
+     else
+        call ed_chi2_fitgf_lattice(Bath_ineq,Weiss,Hloc_ineq,ispin=2)
      endif
-     bath=wmixing*bath + (1.d0-wmixing)*bath_prev
+
+     Bath_ineq=wmixing*Bath_ineq + (1.d0-wmixing)*Bath_prev
+     Bath_prev=Bath_ineq
+
      if(mpiID==0)converged = check_convergence(Weiss(:,1,1,1,1,:),dmft_error,nsuccess,nloop)
 #ifdef _MPI_INEQ
      call MPI_BCAST(converged,1,MPI_LOGICAL,0,MPI_COMM_WORLD,mpiERR)
 #endif
+
   end do
 
   if(mpiID==0)call build_eigenbands()
@@ -153,6 +182,23 @@ program ed_bhz_2d_edge
 #endif
 
 contains
+
+
+
+  function ilat2ineq(ilat) result(ineq)
+    integer,intent(in) :: ilat
+    integer            :: ineq
+    ineq=ilat
+    if( lrsym .AND. (ilat>Nineq) )ineq=Nlat-ilat+1
+  end function ilat2ineq
+
+
+  function ineq2ilat(ineq) result(ilat)
+    integer,intent(in) :: ineq
+    integer            :: ilat
+    ilat=ineq
+    if(ineq>Nineq)stop "ineq2ilat error: called with ineq > Nineq"
+  end function ineq2ilat
 
 
 
@@ -196,37 +242,6 @@ contains
     !SETUP THE LOCAL PART Hloc(Ry)
     allocate(bhzHloc(Nlso,Nlso))
     bhzHloc = extract_Hloc(Hkr,Nlat,Nspin,Norb)
-    !
-    !     !Solve H(kx,Ry) ALONG A -pi:pi PATH
-    !     if(mpiID==0)call build_eigenbands()
-    ! #ifdef _MPI_INEQ
-    !     call MPI_Barrier(MPI_COMM_WORLD,mpiERR)
-    ! #endif
-    !
-    !
-    ! THIS CAN BE DONE SEPARATELY IN THE TIGHT-BINDING CODE:
-    ! ! compute the local gf:
-    ! Smats=zero
-    ! Sreal=zero
-    ! wm = pi/beta*(2*arange(1,Lmats)-1)
-    ! wr = linspace(wini,wfin,Lreal)
-    ! call ed_get_gloc_lattice(Hkr,Wtk,Gmats,Greal,Smats,Sreal,iprint=0,tridiag=tridiag)
-    ! open(10,file="density_"//reg(txtfy(tridiag))//".nint")
-    ! open(11,file="rho_"//reg(txtfy(tridiag))//".nint")
-    ! do iy=1,Ly
-    !    ispin=1
-    !    do iorb=1,Norb
-    !       call splot("Gloc_l"//reg(txtfy(iorb))//reg(txtfy(iorb))//"_"//reg(txtfy(iy,4))//"_iw.nint",wm,Gmats(iy,ispin,ispin,iorb,iorb,:))
-    !       call splot("Gloc_l"//reg(txtfy(iorb))//reg(txtfy(iorb))//"_"//reg(txtfy(iy,4))//"_realw.nint",wr,&
-    !            -dimag(Greal(iy,ispin,ispin,iorb,iorb,:))/pi,dreal(Greal(iy,ispin,ispin,iorb,iorb,:)))
-    !       dens(iy,iorb) = fft_get_density(Gmats(iy,ispin,ispin,iorb,iorb,:),beta)
-    !       rho(iy,iorb)  = -dimag(Gmats(iy,ispin,ispin,iorb,iorb,1))/pi
-    !    enddo
-    !    write(10,"(I4,1000F20.12)")iy,(dens(iy,iorb),iorb=1,Norb)
-    !    write(11,"(I4,1000F20.12)")iy,(rho(iy,iorb),iorb=1,Norb)
-    ! enddo
-    ! close(10)
-    ! close(11)
   end subroutine build_hkr
 
 
