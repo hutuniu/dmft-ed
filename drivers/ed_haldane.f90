8667cb518192cbd5c20533c0dde5c12de55fabdd
Adriano Amaricci <adriano.amaricci@gmail.com>
2016-10-04 17:21:06 +0200

    v4.4.1 UPDATE:
    - updated ed_haldane model. Tests gives correct results.
    - introduced ed_haldane_sc model. Attractive Haldane-Hubbard model. Tests
    are running. Initial resuls are correct. It remains unclear how
    to evaluate the Chern Number in  the superconducting order.

--- drivers/ed_haldane.f90
+++ drivers/ed_haldane.f90
@@ -2,9 +2,7 @@ program ed_haldane
   USE DMFT_ED
   USE SCIFOR
   USE DMFT_TOOLS
-#ifdef _MPI
-  USE MPI
-#endif
+
   implicit none
 
   integer                                       :: iloop,Lk,Nso,Nlso
@@ -24,15 +22,17 @@ program ed_haldane
   complex(8),allocatable,dimension(:,:)         :: halHloc
   complex(8),allocatable,dimension(:,:,:,:,:)   :: Hloc
   real(8),allocatable,dimension(:)              :: Wtk
-  real(8),allocatable,dimension(:)              :: kxgrid
   integer,allocatable,dimension(:)              :: ik2ix,ik2iy
+  real(8)                                       :: a,a0,bklen
+
+  real(8),dimension(2)                          :: d1,d2,d3
   real(8),dimension(2)                          :: a1,a2,a3
-  real(8),dimension(2)                          :: b1,b2,b3
+  real(8),dimension(2)                          :: bk1,bk2,pointK,pointKp
 
   !variables for the model:
   integer                                       :: Nk,Nkpath
   real(8)                                       :: ts,tsp,phi,delta,Mh,wmixing
-  character(len=16)                             :: finput
+  character(len=32)                             :: finput
   character(len=32)                             :: hkfile
   logical                                       :: spinsym
   !
@@ -44,16 +44,8 @@ program ed_haldane
   complex(8),allocatable,dimension(:,:,:,:,:)   :: S0
 
 
-#ifdef _MPI
-  call MPI_INIT(ED_MPI_ERR)
-  call MPI_COMM_RANK(MPI_COMM_WORLD,ED_MPI_ID,ED_MPI_ERR)
-  call MPI_COMM_SIZE(MPI_COMM_WORLD,ED_MPI_SIZE,ED_MPI_ERR)
-  write(*,"(A,I4,A,I4,A)")'Processor ',ED_MPI_ID,' of ',ED_MPI_SIZE,' is alive'
-  call MPI_BARRIER(MPI_COMM_WORLD,ED_MPI_ERR)
-#endif
-
   !Parse additional variables && read Input && read H(k)^4x4
-  call parse_cmd_variable(finput,"FINPUT",default='inputED_BHZ.in')
+  call parse_cmd_variable(finput,"FINPUT",default='inputHALDANE.conf')
   call parse_input_variable(hkfile,"HKFILE",finput,default="hkfile.in")
   call parse_input_variable(nk,"NK",finput,default=100)
   call parse_input_variable(nkpath,"NKPATH",finput,default=500)
@@ -65,26 +57,46 @@ program ed_haldane
   call parse_input_variable(spinsym,"SPINSYM",finput,default=.true.)
   !
   call ed_read_input(trim(finput))
+  !
+  call add_ctrl_var(beta,"BETA")
+  call add_ctrl_var(xmu,"xmu")
+  call add_ctrl_var(wini,"wini")
+  call add_ctrl_var(wfin,"wfin")
+  call add_ctrl_var(eps,"eps")
+  phi=phi*pi
 
   if(Nspin/=1.OR.Norb/=1)stop "Wrong setup from input file: Nspin=Norb=1"
   Nlat=2
   Nso=Nspin*Norb
   Nlso=Nlat*Nso
 
-  !Following:
-  !http://www-personal.umich.edu/~sunkai/teaching/Fall_2012/chapter3_part7.pdf
-  !Lattice basis (a=1) is:
-  !\nu_1 = [ sqrt3  , 0  ]
-  !\nu=2 = [-sqrt3/2,3/2 ]
-  !nearest neighbor: A-->B, B-->A
-  a1 = [0d0           ,  1d0    ]
-  a2 = [-sqrt(3d0)/2d0, -1d0/2d0]
-  a3 = [ sqrt(3d0)/2d0, -1d0/2d0]
+  a=1d0
+  a0=a*sqrt(3d0)
 
+  !Lattice basis (a=1; a0=sqrt3*a) is:
+  !\a_1 = a0 [ sqrt3/2 , 1/2 ]
+  !\a_2 = a0 [ sqrt3/2 ,-1/2 ]
+  !
+  !
+  !nearest neighbor: A-->B, B-->A
+  d1= a*[  1d0/2d0 , sqrt(3d0)/2d0 ]
+  d2= a*[  1d0/2d0 ,-sqrt(3d0)/2d0 ]
+  d3= a*[ -1d0     , 0d0           ]
+  !
   !next nearest-neighbor displacements: A-->A, B-->B \== \nu_1,\nu_2, \nu_3=\nu_1-\nu_2
-  b1=[ sqrt(3d0), 0d0]
-  b2=[-sqrt(3d0)/2d0, 3d0/2d0]
-  b3=[-sqrt(3d0)/2d0,-3d0/2d0]
+  a1=a0*[ sqrt(3d0)/2d0, 1d0/2d0]
+  a2=a0*[ sqrt(3d0)/2d0,-1d0/2d0]
+  a3=a2-a1
+
+
+  !RECIPROCAL LATTICE VECTORS:
+  bklen=4d0*pi/3d0
+  bk1=bklen*[ 1d0/2d0 ,  sqrt(3d0)/2d0 ]
+  bk2=bklen*[ 1d0/2d0 , -sqrt(3d0)/2d0 ]
+
+
+  pointK = [2*pi/3, 2*pi/3/sqrt(3d0)]
+  pointKp= [2*pi/3,-2*pi/3/sqrt(3d0)]
 
 
 
@@ -104,47 +116,47 @@ program ed_haldane
   Hloc = lso2nnn_reshape(halHloc,Nlat,Nspin,Norb)
 
   !Setup solver
-  Nb=get_bath_size()
+  Nb=get_bath_dimension()
   allocate(Bath(Nlat,Nb))
   allocate(Bath_prev(Nlat,Nb))
-  call ed_init_solver_lattice(Bath)
+  call ed_init_solver(Bath)
 
 
   !DMFT loop
   iloop=0;converged=.false.
   do while(.not.converged.AND.iloop<nloop)
      iloop=iloop+1
-     if(ED_MPI_ID==0)call start_loop(iloop,nloop,"DMFT-loop")
+     call start_loop(iloop,nloop,"DMFT-loop")
 
      !Solve the EFFECTIVE IMPURITY PROBLEM (first w/ a guess for the bath)
-     call ed_solve_lattice(bath,Hloc,iprint=1)
-
+     call ed_solve(bath,Hloc,iprint=1)
      call ed_get_sigma_matsubara_lattice(Smats,Nlat)
      call ed_get_sigma_real_lattice(Sreal,Nlat)
+     S0 = dreal(Smats(:,:,:,:,:,1))
+     call ChernNumber()
+     call build_EigenBands()
 
      ! compute the local gf:
-     call ed_get_gloc_lattice(Hk,Wtk,Gmats,Greal,Smats,Sreal,iprint=1)
+     call dmft_gloc_matsubara(Hk,Wtk,Gmats,Smats,iprint=4)
+     call dmft_gloc_realaxis(Hk,Wtk,Greal,Sreal,iprint=4)
 
      ! compute the Weiss field (only the Nineq ones)
-     call ed_get_weiss_lattice(Gmats,Smats,Weiss,Hloc,iprint=1)
+     if(cg_scheme=='weiss')then
+        call dmft_weiss(Gmats,Smats,Weiss,Hloc,iprint=4)
+     else
+        call dmft_delta(Gmats,Smats,Weiss,Hloc,iprint=4)
+     endif
 
      !Fit the new bath, starting from the old bath + the supplied Weiss
-     call ed_chi2_fitgf_lattice(Bath,Weiss,Hloc,ispin=1)
-
+     call ed_chi2_fitgf(Bath,Weiss,Hloc,ispin=1)
 
      !MIXING:
      if(iloop>1)Bath=wmixing*Bath + (1.d0-wmixing)*Bath_prev
      Bath_prev=Bath
-     if(ED_MPI_ID==0)converged = check_convergence(Weiss(:,1,1,1,1,:),dmft_error,nsuccess,nloop)
-#ifdef _MPI
-     call MPI_BCAST(converged,1,MPI_LOGICAL,0,MPI_COMM_WORLD,ED_MPI_ERR)
-#endif
-     if(ED_MPI_ID==0)call end_loop
-  enddo
+     converged = check_convergence(Weiss(:,1,1,1,1,:),dmft_error,nsuccess,nloop)
 
-#ifdef _MPI
-  call MPI_FINALIZE(ED_MPI_ERR)
-#endif
+     call end_loop
+  enddo
 
 
 
@@ -157,25 +169,26 @@ contains
   !Haldane HAMILTONIAN:
   !--------------------------------------------------------------------!
   function hk_haldane_model(kpoint,Nlso) result(hk)
-    real(8),dimension(:)            :: kpoint
-    integer                         :: Nlso
+    real(8),dimension(:)          :: kpoint
+    integer                       :: Nlso
     complex(8),dimension(Nlso,Nlso) :: hk
-    real(8)                         :: h0,hx,hy,hz
-    real(8)                         :: kdota(3),kdotb(3)
+    real(8)                       :: h0,hx,hy,hz
+    real(8)                       :: kdotd(3),kdota(3)
+    !(k.d_j)
+    kdotd(1) = dot_product(kpoint,d1)
+    kdotd(2) = dot_product(kpoint,d2)
+    kdotd(3) = dot_product(kpoint,d3)
     !(k.a_j)
     kdota(1) = dot_product(kpoint,a1)
     kdota(2) = dot_product(kpoint,a2)
     kdota(3) = dot_product(kpoint,a3)
-    !(k.b_j)
-    kdotb(1) = dot_product(kpoint,b1)
-    kdotb(2) = dot_product(kpoint,b2)
-    kdotb(3) = dot_product(kpoint,b3)
     !
-    h0 = 2*tsp*cos(phi)*sum( cos(kdotb(:)) )
-    hx =-ts*sum( cos(kdota(:)) )
-    hy =-ts*sum( sin(kdota(:)) )
-    hz = 2*tsp*sin(phi)*sum( sin(kdotb(:)) ) + Mh 
+    h0 = -2*tsp*cos(phi)*sum( cos(kdota(:)) )
+    hx =-ts*sum( cos(kdotd(:)) )
+    hy =-ts*sum( sin(kdotd(:)) )
+    hz = -2*tsp*sin(phi)*sum( sin(kdota(:)) ) + Mh 
     hk = h0*pauli_0 + hx*pauli_x + hy*pauli_y + hz*pauli_z
+    hk = hk  -  nnn2lso_reshape(S0,Nlat,Nspin,Norb)
   end function hk_haldane_model
 
 
@@ -187,143 +200,172 @@ contains
   !PURPOSE: Get Haldane Model Hamiltonian
   !---------------------------------------------------------------------
   subroutine build_hk(file)
-    character(len=*),optional           :: file
-    integer                             :: i,j,ik=0
-    integer                             :: ix,iy
-    real(8)                             :: kx,ky    
-    integer                             :: iorb,jorb
-    integer                             :: isporb,jsporb
-    integer                             :: ispin,jspin
-    real(8)                             :: foo
-    integer                             :: unit
-    complex(8),dimension(Nlso,Nlso,Lmats) :: Gmats
-    complex(8),dimension(Nlso,Nlso,Lreal) :: Greal
-    real(8)                             :: wm(Lmats),wr(Lreal),dw,n0(Nlso)
-
-    call build_hk_path()
-
-    if(ED_MPI_ID==0)write(LOGfile,*)"Build H(k) for BHZ:"
+    character(len=*),optional             :: file
+    integer                               :: i,j,ik
+    integer                               :: ix,iy
+    real(8)                               :: kx,ky    
+    integer                               :: iorb,jorb
+    integer                               :: isporb,jsporb
+    integer                               :: ispin,jspin
+    real(8)                               :: foo,n0(Nlso)
+    integer                               :: unit
+    complex(8),dimension(Nlso,Nlso,Lmats) :: Gmats,fooSmats
+    complex(8),dimension(Nlso,Nlso,Lreal) :: Greal,fooSreal
+    real(8),dimension(:),allocatable      :: kxgrid,kygrid
+    real(8),dimension(2)                  :: kvec
+    real(8),dimension(:,:),allocatable    :: kpath
+    write(LOGfile,*)"Build H(k) for Haldane model (now also Nobel Laureate):"
     Lk=Nk*Nk
-    if(ED_MPI_ID==0)write(*,*)"# of k-points     :",Lk
-    if(ED_MPI_ID==0)write(*,*)"# of SO-bands     :",Nlso
+    write(*,*)"# of k-points     :",Lk
+    write(*,*)"# of SO-bands     :",Nlso
     !
     if(allocated(Hk))deallocate(Hk)
     if(allocated(wtk))deallocate(wtk)
     !
     allocate(Hk(Nlso,Nlso,Lk))
-    allocate(wtk(Lk))
-    allocate(kxgrid(Nk))
-    kxgrid = kgrid(Nk)
-    Hk     = TB_build_model(hk_haldane_model,Nlso,kxgrid,kxgrid,[0d0])
-    wtk    = 1d0/Lk
-    if(ED_MPI_ID==0.AND.present(file))then
-       call write_hk_w90("Hkrfile_Haldane.data",&
-            No=Nlso,&
-            Nd=Norb,&
-            Np=0,&
-            Nineq=1,&
-            Hk=Hk,&
-            kxgrid=kxgrid,kygrid=kxgrid,kzgrid=[0d0])
-    endif
+    allocate(Wtk(Lk))
+    print*,"Build Hk(Nlso,Nlso) for the Graphene model"
+    ik=0
+    do iy=1,Nk
+       ky = dble(iy-1)/Nk
+       do ix=1,Nk
+          ik=ik+1
+          kx=dble(ix-1)/Nk
+          kvec = kx*bk1 + ky*bk2
+          Hk(:,:,ik) = hk_haldane_model(kvec,Nlso)
+       enddo
+    enddo
+    Wtk = 1d0/Lk
     !
     allocate(halHloc(Nlso,Nlso))
     halHloc = sum(Hk(:,:,:),dim=3)/Lk
     where(abs(dreal(halHloc))<1.d-9)halHloc=0d0
-    if(ED_MPI_ID==0)call write_Hloc(halHloc)
+    call write_Hloc(halHloc)
     !
+    !
+    allocate(Kpath(4,2))
+    KPath(1,:)=[0,0]
+    KPath(2,:)=pointK
+    Kpath(3,:)=pointKp
+    KPath(4,:)=[0d0,0d0]
+    call TB_Solve_path(hk_haldane_model,Nlso,KPath,Nkpath,&
+         colors_name=[red1,blue1],&
+         points_name=[character(len=10) :: "G","K","K`","G"],&
+         file="Eigenbands.nint")
+    !
+    !
+    call ChernNumber()
     !
     !Build the local GF:
-    wm = pi/beta*real(2*arange(1,Lmats)-1,8)
-    wr = linspace(wini,wfin,Lreal,mesh=dw)
-    do ik=1,Lk
-       do i=1,Lmats
-          Gmats(:,:,i)=Gmats(:,:,i) + inverse_g0k( xi*wm(i)+xmu,Hk(:,:,ik))/Lk
-       enddo
-       do i=1,Lreal
-          Greal(:,:,i)=Greal(:,:,i) + inverse_g0k(dcmplx(wr(i),eps)+xmu,Hk(:,:,ik))/Lk
-       enddo
-    enddo
+    fooSmats=zero
+    call dmft_gloc_matsubara(Hk,Wtk,Gmats,fooSmats,iprint=4)
+    fooSreal=zero
+    call dmft_gloc_realaxis(Hk,Wtk,Greal,fooSreal,iprint=4)
     do iorb=1,Nlso
-       call splot("G0loc_l"//reg(txtfy(iorb))//"m"//reg(txtfy(iorb))//"_iw.ed",wm,Gmats(iorb,iorb,:))
-       call splot("G0loc_l"//reg(txtfy(iorb))//"m"//reg(txtfy(iorb))//"_realw.ed",wr,&
-            -dimag(Greal(iorb,iorb,:))/pi,dreal(Greal(iorb,iorb,:)))
        n0(iorb) = fft_get_density(Gmats(iorb,iorb,:),beta)
     enddo
+    open(10,file="density.dat")
+    write(10,"(10F20.12)")(n0(iorb),iorb=1,Nlso),sum(n0)
+    close(10)
     !
   end subroutine build_hk
 
 
 
 
-  !---------------------------------------------------------------------
-  !PURPOSE: solve H(k) along a specified path in the BZ
-  !---------------------------------------------------------------------
-  subroutine build_hk_path(kpath_)
-    integer                            :: i,j
-    integer                            :: Npts
-    real(8),dimension(:,:),optional    :: kpath_
-    real(8),dimension(:,:),allocatable :: kpath
-    character(len=64)                  :: file
-
-    if(present(kpath_))then
-       if(ED_MPI_ID==0)write(LOGfile,*)"Build H(k) along a given path:"
-       Npts = size(kpath_,1)
-       Lk=(Npts-1)*Nkpath
-       allocate(kpath(Npts,size(kpath_,2)))
-       kpath=kpath_
-       file="bands_path"
-    else
-       if(ED_MPI_ID==0)write(LOGfile,*)"Build H(k) along the path G-K-K`-G:"
-       Npts = 4
-       Lk=(Npts-1)*Nkpath
-       allocate(kpath(Npts,3))
-       kpath(1,:)=kpoint_Gamma
-       kpath(2,:)=[2d0/3d0/sqrt(3d0),0d0]*pi2
-       kpath(3,:)=[1d0/3d0/sqrt(3d0),1/3d0]*pi2
-       kpath(4,:)=kpoint_Gamma
-       file="non_interacting_bands"
-    endif
+  subroutine build_EigenBands()
+    integer                               :: i,j,ik
+    integer                               :: ix,iy
+    real(8)                               :: kx,ky    
+    integer                               :: iorb,jorb
+    integer                               :: isporb,jsporb
+    integer                               :: ispin,jspin
+    real(8)                               :: foo,n0(Nlso)
+    integer                               :: unit
+    complex(8),dimension(Nlso,Nlso,Lmats) :: Gmats,fooSmats
+    complex(8),dimension(Nlso,Nlso,Lreal) :: Greal,fooSreal
+    real(8),dimension(:),allocatable      :: kxgrid,kygrid
+    real(8),dimension(2)                  :: kvec
+    real(8),dimension(:,:),allocatable    :: kpath
     !
-    if(allocated(Hk))deallocate(Hk)
-    if(allocated(wtk))deallocate(wtk)
-    !
-    allocate(Hk(Nlso,Nlso,Lk))
-    allocate(wtk(Lk))
-    Hk  = TB_build_model(hk_haldane_model,Nlso,kpath,Nkpath)
-    wtk = 1d0/Lk
-    if(ED_MPI_ID==0) call TB_Solve_path(hk_haldane_model,Nlso,KPath,Nkpath,&
+    allocate(Kpath(4,2))
+    KPath(1,:)=[0,0]
+    KPath(2,:)=pointK
+    Kpath(3,:)=pointKp
+    KPath(4,:)=[0d0,0d0]
+    call TB_Solve_path(hk_haldane_model,Nlso,KPath,Nkpath,&
          colors_name=[red1,blue1],&
          points_name=[character(len=10) :: "G","K","K`","G"],&
-         file=reg(file))
-  end subroutine build_hk_path
+         file="Eigenbands.dat")
+    !
+    !
+  end subroutine build_EigenBands
 
 
 
+  subroutine ChernNumber
+    complex(8),dimension(:,:,:),allocatable :: BlochStates
+    real(8),dimension(:,:),allocatable      :: Berry_curvature
+    integer                                 :: ik,ix,iy
+    complex(8)                              :: Eigvec(Nlso,Nlso)
+    real(8)                                 :: Chern,BZ_area,eigval(Nlso)
+    real(8),dimension(:),allocatable        :: kxgrid
+    !CHERN NUMBERS:
+    allocate(BlochStates(Nlso,Nk,Nk))
+    allocate(Berry_curvature(Nk,Nk))
+    chern=0d0
+    ik=0
+    do ix=1,Nk
+       do iy=1,Nk
+          ik=ik+1
+          Eigvec = Hk(:,:,ik) -  nnn2lso_reshape(S0,Nlat,Nspin,Norb)
+          call eigh(Eigvec,Eigval)
+          BlochStates(:,ix,iy) = Eigvec(:,1)
+       enddo
+    enddo
+    call get_Chern_number(BlochStates,Chern,Berry_curvature,Nk/pi2*Nk/pi2)
+    write(LOGfile,*)"Chern =",chern
+    allocate(kxgrid(Nk))
+    kxgrid=linspace(0d0,pi2,Nk)
+    call splot3d("Berry_Curvature.nint",kxgrid,kxgrid,Berry_Curvature)
+    open(10,file="ChernNumber.dat")
+    write(10,*)int(chern)
+    close(10)
+  end subroutine ChernNumber
 
 
+  subroutine Get_Chern_number(State,Chern_number,Berry_curvatures,one_over_area)
+    complex(8),intent(in),dimension(:,:,:)                     :: state !(nhilb, nk1, nk2)
+    real(8),intent(out)                                        :: chern_number
+    real(8),intent(out),dimension(size(state,2),size(state,3)) :: berry_curvatures
+    real(8),intent(in)                                         :: one_over_area
+    integer                                                    :: nhilb,nk1,nk2
+    integer                                                    :: i1,i2,i3,ix,i1p,i1m,i2m,i2p,it
+    complex(8)                                                 :: path_scalar_products(4)
+    real(8)                                                    :: berry_phase
+    !
+    Nhilb= size(state,1)
+    Nk1  = size(state,2)
+    Nk2  = size(state,3)
+    chern_number = zero
+    do i1= 1, nk1
+       i1p = modulo(i1,nk1) + 1
+       i1m = modulo(i1-2,nk1) + 1
+       do i2= 1, nk2           !faccio l'integrale sulla bz
+          i2p = modulo(i2,nk2) + 1
+          i2m = modulo(i2-2,nk2) + 1
+          path_scalar_products(1) = dot_product(state(:,i1,i2),state(:,i1, i2p))
+          path_scalar_products(2) = dot_product(state(:,i1,i2p),state(:,i1p, i2p))
+          path_scalar_products(3) = dot_product(state(:,i1p,i2p),state(:,i1p, i2))
+          path_scalar_products(4) = dot_product(state(:,i1p,i2),state(:,i1,i2))
+          berry_phase = -dimag(zlog( product(path_scalar_products)  ))
+          berry_curvatures(i1,i2) = berry_phase*one_over_area
+          chern_number = chern_number + berry_phase
+       enddo
+    enddo
+    chern_number = chern_number/pi2
+  end subroutine Get_Chern_number
 
-
-
-
-
-
-
-
-  function inverse_g0k(zeta,hk) result(gk)
-    integer                     :: i
-    complex(8),dimension(2,2)   :: hk
-    complex(8)                  :: zeta
-    complex(8),dimension(2,2)   :: gk
-    Complex(8)                  :: delta,ppi,vmix
-    gk=zero
-    delta = zeta - hk(1,1)
-    ppi   = zeta - hk(2,2)
-    vmix  = -hk(1,2)
-    gk(1,1) = one/(delta - abs(vmix)**2/ppi)
-    gk(2,2) = one/(ppi - abs(vmix)**2/delta)
-    gk(1,2) = -vmix/(ppi*delta - abs(vmix)**2)
-    gk(2,1) = -conjg(vmix)/(ppi*delta - abs(vmix)**2)
-  end function inverse_g0k
 
 
 end program ed_haldane
